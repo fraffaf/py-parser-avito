1da2d26bb1f305c38cc822104078fd99bc694c20
fraffaf <ilya.gi99@gmail.com>
2021-09-29 01:31:26 +0300

    Initial commit

---
+++ pareser/parser.py
@@ -0,0 +1,18 @@
+# Python avito parser 1.0
+
+import requests
+import ipdb
+
+proxy = {"http": "localhost:8080", "https": "localhost:8080"}
+
+headers = {
+    "User-Agent": "Chrome"
+}
+
+r = requests.get("https://ya.ru", proxies=proxy, verify=False)
+
+if r.ok:            # if r.status = 200
+    print("ok")     # print("ok")
+
+
+
